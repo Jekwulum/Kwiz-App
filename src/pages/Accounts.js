87b39a4c22761d002ee89fe3a8507d6f017ef4b5
Charles <charlesnwoye2@gmail.com>
2022-12-31 19:20:51 +0100

    implemented footer component

--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/footer/Footer';
 import SideMenu from '../components/sideMenu/SideMenu';
 
 const Accounts = () => {
@@ -8,16 +9,19 @@ const Accounts = () => {
 
       <div>
 
-        {/* Header goes here */}
+        <div className='h-screen'>
 
-        <div className='p-7 font-semibold flex-1 h-screen'>
-          <h1 className='text-4xl'>Accounts</h1>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil a rerum eaque maxime id. Sed aut, mollitia maxime dolores pariatur magni corporis eveniet eaque labore quas velit aliquid eligendi repellendus tempore, ipsam repellat in modi nulla culpa, sapiente enim? Quia necessitatibus quod, praesentium soluta obcaecati nostrum. Ut debitis esse eius!</p>
+          {/* Header goes here */}
+
+          <div className='p-7 font-semibold flex-1'>
+            <h1 className='text-4xl'>Accounts</h1>
+            <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil a rerum eaque maxime id. Sed aut, mollitia maxime dolores pariatur magni corporis eveniet eaque labore quas velit aliquid eligendi repellendus tempore, ipsam repellat in modi nulla culpa, sapiente enim? Quia necessitatibus quod, praesentium soluta obcaecati nostrum. Ut debitis esse eius!</p>
+            </div>
           </div>
-        </div>
 
-        {/* Footer */}
+          <Footer />
+        </div>
 
       </div>
     </div>
