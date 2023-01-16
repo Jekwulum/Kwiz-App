561cf61a2fd38d0c21466a13064ba38809a3938c
Charles <charlesnwoye2@gmail.com>
2023-01-16 16:44:43 +0100

    login page added with responsiveness

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,10 +3,35 @@ import LoginPhoto from '../assets/images/login.svg';
 
 const Login = () => {
   return (
-    <div>Login
-        <div>
-            <img src={LoginPhoto} className="h-75 w-32"/>
-        </div>
+    <div className='flex md:flex-row items-center justify-center mx-auto h-screen space-x-6'>
+
+      <div className='hidden md:block'>
+        <img src={LoginPhoto} className="h-96 w-64" />
+      </div>
+
+      <div className=''>
+        <form className='flex flex-col justify-center items-center'>
+          <p className='text-3xl m-4 text-light-bg'>User Login</p>
+
+          <input placeholder='email' type='email' className='m-2 w-64 h-10 rounded-full text-center focus:outline-none text-sm placeholder-light-bg text-light-bg bg-gray-200' />
+
+          <input placeholder='password' type='password' className='m-2 w-64 h-10 rounded-full text-center focus:outline-none text-sm placeholder-light-bg text-light-bg bg-gray-200' />
+
+          <div className='flex space-x-10 text-xs text-light-bg m-2 items-center text-center'>
+
+            <span className='flex items-center'>
+              <input id="checkbox_id" type='checkbox' className='focus:outline-none m-1 hover:cursor-pointer text-light-bg' />
+              <label htmlFor='checkbox_id' className='hover:cursor-pointer'>remember me</label>
+            </span>
+
+            <a href='#' className='underline'>Forgot Password?</a>
+            
+          </div>
+
+          <button type='submit' className='w-64 h-10 m-2 mt-4 rounded-full text-sm text-cust-light bg-light-bg'>Login</button>
+        </form>
+      </div>
+
     </div>
   )
 }
