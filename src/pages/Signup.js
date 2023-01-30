72ef5c505d516381852bd2f84740d05b5511e600
Charles <charlesnwoye2@gmail.com>
2023-01-30 15:50:28 +0100

    form validation complete

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,27 +5,30 @@ import AuthService from '../utils/services/auth.service';
 import encryptHelper from '../utils/helpers/encryptHelper';
 import tokenHelper from '../utils/helpers/tokenHelper';
 import { Loading } from '../utils/helpers/constants';
+import { validateEmail } from '../utils/helpers/validators';
 
 const Signup = () => {
   const [passwordType, setPasswordType] = useState("password");
-  const [confirmPasswordType, setConfirmPasswordType] = useState("password");
+  const [re_passwordType, setRePasswordType] = useState("password");
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [re_password, setRePassword] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoadingStatus] = useState(false);
   const [emailValidator, setEmailValidator] = useState(null);
+
+  const isDisabled = !lastName || !firstName || !email || !validateEmail(email) || !password || !re_password || !(re_password === password);
 
   const togglePasswordField = type => {
     setPasswordType(type);
     document.getElementById('passwordMain').type = type;
   };
 
-  const toggleConfirmPasswordField = type => {
-    setConfirmPasswordType(type);
-    document.getElementById('confirmPassword').type = type;
+  const togglere_passwordField = type => {
+    setRePasswordType(type);
+    document.getElementById('re_password').type = type;
   };
 
   const handleEmailValidator = event => {
@@ -36,6 +39,11 @@ const Signup = () => {
       setEmailValidator(null);
       setEmail(email);
     }
+  };
+
+  const handleSignUp = async () => {
+    let payload = { firstName, lastName, email, password, re_password };
+    console.log(payload);
   };
 
   return (
@@ -51,7 +59,7 @@ const Signup = () => {
           <input placeholder='email' type='email'
             value={email} onChange={e => setEmail(e.target.value)} onInput={handleEmailValidator}
             className='m-2 w-64 h-10 rounded-full text-center focus:outline-none text-sm placeholder-light-bg text-light-bg bg-gray-200' />
-            {emailValidator ? <small className="text-red-700">{emailValidator}</small> : ''}
+          {emailValidator ? <small className="text-red-700">{emailValidator}</small> : ''}
 
           <input placeholder='First name' type='text'
             value={firstName} onChange={e => setFirstName(e.target.value)}
@@ -68,28 +76,28 @@ const Signup = () => {
             <div className='h-0 eye-right relative left-60 bottom-10 text-light-bg'>
               {
                 passwordType === "password" ?
-                  <i onClick={() => togglePasswordField('text')} className='uil uil-eye'></i> :
-                  <i onClick={() => togglePasswordField('password')} className='uil uil-eye-slash'></i>
+                  <i onClick={() => togglePasswordField('text')} className='uil uil-eye hover:cursor-pointer'></i> :
+                  <i onClick={() => togglePasswordField('password')} className='uil uil-eye-slash hover:cursor-pointer'></i>
               }
             </div>
           </div>
 
           <div>
-            <input placeholder='confirm password' type={confirmPasswordType} value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} id={'confirmPassword'}
+            <input placeholder='confirm password' type={re_passwordType} value={re_password} onChange={e => setRePassword(e.target.value)} id={'re_password'}
               className='m-2 w-64 h-10 rounded-full text-center focus:outline-none text-sm placeholder-light-bg text-light-bg bg-gray-200' />
 
             <div className='h-0 eye-right relative left-60 bottom-10 text-light-bg'>
               {
-                confirmPasswordType === "password" ?
-                  <i onClick={() => toggleConfirmPasswordField('text')} className='uil uil-eye'></i> :
-                  <i onClick={() => toggleConfirmPasswordField('password')} className='uil uil-eye-slash'></i>
+                re_passwordType === "password" ?
+                  <i onClick={() => togglere_passwordField('text')} className='uil uil-eye hover:cursor-pointer'></i> :
+                  <i onClick={() => togglere_passwordField('password')} className='uil uil-eye-slash hover:cursor-pointer'></i>
               }
             </div>
           </div>
-          {password !== confirmPassword ? <small className="text-red-700">Passwords do not match!</small> : ''}
+          {password !== re_password ? <small className="text-red-700">Passwords do not match!</small> : ''}
 
-          <button type='submit'
-            className='w-64 h-10 m-2 mt-4 rounded-full text-sm text-cust-light bg-light-bg'>
+          <button type='submit' disabled={isDisabled} onClick={handleSignUp}
+            className={`w-64 h-10 m-2 mt-4 rounded-full text-sm text-cust-light bg-light-bg ${isDisabled ? 'opacity-50' : ''}`}>
             Create {loading ? <>&nbsp;<i class="fa fa-spinner fa-spin mx-1"></i></> : ""}</button>
 
           <p className='text-xs'>Already have an account? <a href="/login" className='text-light-bg'>Sign in here</a></p>
