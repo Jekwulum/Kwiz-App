c8a1e0abf2a50a6709a810f006161526c744707b
Charles <charlesnwoye2@gmail.com>
2023-01-30 11:40:39 +0100

    added responsiveness for input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import HomePhoto from '../assets/images/home_photo.svg';
 
 const Home = () => {
   return (
-    <div>
+    <div className=''>
 
       <div className='flex justify-between my-6 mx-3'>
         <p className='text-5xl font-bold text-light-bg'>Kwiz</p>
@@ -15,8 +16,12 @@ const Home = () => {
 
       <div className='h-32 mx-auto text-center'>
         <input type="text" placeholder='Enter Quiz Code'
-          className='w-72 h-9 m-2 p-4 border-2 outline-none align-middle text-center text-light-bg'/>
+          className='w-3/6 md:w-80 h-9 m-2 p-4 border-2 outline-none align-middle text-center text-light-bg'/>
         <button className='bg-light-bg h-11 w-11 mt-2 rounded-full text-white font-bold text-lg'>Go</button>
+      </div>
+
+      <div className='flex text-center items-center mx-auto'>
+        <img src={HomePhoto} alt="" className='mx-auto h-72' />
       </div>
 
     </div>
