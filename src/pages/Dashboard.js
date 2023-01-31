d313287fecfbf2f41ae06af2e3e640e8bab0932a
Charles <charlesnwoye2@gmail.com>
2023-01-31 18:23:02 +0100

    screen size adjusted

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -23,24 +23,26 @@ const Dashboard = () => {
     batch(() => {
       if (loadingUser !== Loading.SUCCESS) dispatch(fetchUserById(userId))
     });
-  }, [dispatch, loadingUser]);
+  }, [dispatch]);
+
+  console.log("user:::: ", user);
 
   return (
     <div className='flex'>
       <SideMenu />
 
-      <div>
-        <div className='h-screen'>
+      {/* <div> */}
+        <div className='h-screen w-full'>
           <Header />
           <div className='p-7 h-5/6 font-semibold flex-1 dark:bg-cust-dark-body dark:text-cust-light'>
             <h1 className='text-4xl'>Dashboard</h1>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil a rerum eaque maxime id. Sed aut, mollitia maxime dolores pariatur magni corporis eveniet eaque labore quas velit aliquid eligendi repellendus tempore, ipsam repellat in modi nulla culpa, sapiente enim? Quia necessitatibus quod, praesentium soluta obcaecati nostrum. Ut debitis esse eius!</p>
+            <div className=''>
+              <p>Welcome Here</p>
             </div>
           </div>
           <Footer />
         </div>
-      </div>
+      {/* </div> */}
     </div>
   )
 }
