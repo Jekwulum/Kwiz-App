0debd33a7c74be7e67fd9366c9b80066ea0c51b3
Charles <charlesnwoye2@gmail.com>
2023-06-07 12:18:17 +0100

    design for rendering questions

--- src/pages/atom/RenderQuestionsStats.js
+++ src/pages/atom/RenderQuestionsStats.js
@@ -3,14 +3,17 @@ import React from 'react';
 const RenderQuestionsStats = ({ stats }) => {
   const { players, questions, titles } = stats;
   return (
-    <div>
-      <h1 className='text-3xl text-light-bg text-center md:text-left mt-2'>All Quizzes</h1>
+    <div className='flex flex-col items-center mx-auto'>
+      <h1 className='text-3xl text-light-bg text-center mt-2'>All Quizzes</h1>
       {questions.map((questionObj, questionIndex) => (
-        <div key={questionIndex} className='bg-blue-200 my-2 md:w-2/5 text-light-bg rounded-lg p-3'>
-          <div>Title: {questionObj.title}</div>
-          <div>ID: {questionObj.quizId}</div>
-          <div>No of questions: {titles[questionObj.quizId]}</div>
-          <div>No of players: {players[questionObj.quizId]}</div>
+        <div key={questionIndex}
+          className='flex items-center justify-between w-full md:w-2/5 my-2 text-light-bg rounded-lg p-3 border-2 border-light-bg'>
+          <div>
+            <div className='text-gray-500'>{titles[questionObj.quizId]} question{titles[questionObj.quizId] > 1 ? "s" : ""}</div>
+            <div className='text-2xl text-black'>{questionObj.title}</div>
+            {/* <div>ID: {questionObj.quizId}</div> */}
+            <div className='text-light-bg'>{players[questionObj.quizId]} player{players[questionObj.quizId] == 1 ? "" : "s"}</div>
+          </div>
 
           <button className='bg-light-bg h-10 w-24 mt-1 rounded-lg text-white p-2 text-xs'>View</button>
         </div>
